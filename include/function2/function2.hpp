141f1f6c3a2dc1ecded17f51600a5e3c47d62d72
Иван <dvvn@ya.ru>
2022-08-19 12:50:51 +0300

    sync

--- include/function2/function2.hpp
+++ include/function2/function2.hpp
@@ -1074,9 +1074,8 @@ struct internal_capacity {
     /// Tag to access the structure in a type-safe way
     data_accessor accessor_;
     /// The internal capacity we use to allocate in-place
-    /// std::aligned_storage_t<Capacity::capacity, Capacity::alignment>
-    /// capacity_;
-    alignas(Capacity::alignment) std::byte t_buff[apacity::capacity] capacity_;
+    /// std::aligned_storage_t<Capacity::capacity, Capacity::alignment> capacity_;
+    alignas(Capacity::alignment) std::byte capacity_[apacity::capacity];
   } type;
 };
 template <typename Capacity>
